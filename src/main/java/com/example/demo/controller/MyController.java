ffc3425e81be6035cb2e6b9e7f6765903943f544
Yevhenii Donenko <Yevhenii_Donenko@epam.com>
2022-06-24 11:49:09 +0300

    1

--- src/main/java/com/example/demo/controller/MyController.java
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MyController {
     @GetMapping("/hello")
     public String hello() {
+        System.out.println("controller");
         return "man";
     }
 
